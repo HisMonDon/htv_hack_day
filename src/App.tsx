645840e1de07d895b02ac5dcd4cf86d9f26355ff
Andre Sidarous <andi.sidarous@gmail.com>
2026-07-25 16:17:59 -0400

    arcade audio added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import { useState } from "react";
 import { LobbyView } from "./components/LobbyView";
 import { StartMenu } from "./components/StartMenu";
+import music from "./audio/music.mp3";
+
+let gameMusic: HTMLAudioElement | null = null;
+
+function startMusic() {
+  if (!gameMusic) {
+    gameMusic = new Audio(music);
+    gameMusic.loop = true;
+    gameMusic.volume = 0.4;
+  }
+
+  gameMusic.play().catch((err) => {
+    console.log("Music failed:", err);
+  });
+}
 
 export function App() {
   const [started, setStarted] = useState(false);
 
-  return started ? <LobbyView onMenu={() => setStarted(false)} /> : <StartMenu onStart={() => setStarted(true)} />;
+  const handleStart = () => {
+    startMusic();
+    setStarted(true);
+  };
+
+  return started ? (
+    <LobbyView onMenu={() => setStarted(false)} />
+  ) : (
+    <StartMenu onStart={handleStart} />
+  );
 }
