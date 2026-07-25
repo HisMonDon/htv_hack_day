d4cc3dec8552d637b26f2140f3990b562de46394
HisMonDon <revenge.ez.123@gmail.com>
2026-07-25 10:11:46 -0400

    Stop tracking Vite environment declarations

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_GEMINI_API_KEY: string | undefined;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
